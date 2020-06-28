8c901b2268d1baa78ebc0655a98fe489d9998a18
mott4a <taua.gamer@gmail.com>
2020-06-28 19:20:16 -0300

    28/06 adcionado contato.html e js do slide refatorado e diminuido

--- src/js/posts.js
+++ src/js/posts.js
@@ -1,24 +1,12 @@
-var post = {
-    "post": [{
-      "id": "randomId1",
-      "link": "clickbait.com"
-    }, {
-      "id": "randomId2",
-      "link": "imgur.com"
-    }]
-  };
+// fetch("../../posts.json")
+//   .then(res => res.json())
+//     .then(data => {
+//     console.log(data)
+//     })
+//     .catch(err => console.error(err));
 
-    var parentDiv = document.getElementById('parentDiv');
-    yourJsonObject.points.forEach(function(item, index){
-            var newDiv = document.createElement('div');
-            newDiv.id = "post" + index;
-    
-            var pChild = document.createElement('p');
-            pChild.id = "link";
-            //if so required
-            //replace above with 'pChild.id = item.link;'
-    
-    
-            newDiv.appendChild(pChild);
-            parentDiv.appendChild(newDiv);
-        });
+fetch("../../posts.json",{
+}).then(res => {
+  return res.json()
+}).then(data => console.log(data))
+  .catch(error => console.log('deu merda'));
